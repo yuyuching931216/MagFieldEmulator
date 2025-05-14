0b0d2852bbfcd18f0b55f65cc064364e976d75c9
yuyuching931216 <yuchingliu93@gmail.com>
2025-05-14 21:01:39 +0800

    Update nt_to_volt conversion factor in AppConfig for improved accuracy

--- app_config.py
+++ app_config.py
@@ -7,7 +7,7 @@ class AppConfig:
     csv_folder: str = "data"
     csv_log: str = "output_log.csv"
     device_name: str = "Dev1"
-    nt_to_volt: float = 1.0 / 10000  # 1V = 10,000nT
+    nt_to_volt: float = 1.0 / 100000  # 1V = 10,000nT
     interval: float = 60.0  # 每 60 秒輸出一次
     log_flush_interval: int = 10  # 每處理10筆數據寫入一次日誌
 
